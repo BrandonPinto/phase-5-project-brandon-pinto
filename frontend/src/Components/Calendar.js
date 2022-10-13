bb1a9d379e6347c6131a6e444321fa4f2437d9b1
Brandon <b.pintony@gmail.com>
2022-10-12 21:29:20 -0400

    calendar attempt 1

--- frontend/src/Components/Calendar.js
+++ frontend/src/Components/Calendar.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import FullCalendar from '@fullcalendar/react'
+import FullCalendar, {formatDate} from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-
+import listPlugin from '@fullcalendar/list'
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+    useTheme
+} from "@mui/material"
 
 
 export default function Calendar() {
